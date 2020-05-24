772585758cf70e36a029e781940735f77f69f8e3
Joshua Mwakio <mwakiojoshua@hotmail.com>
2020-05-24 20:54:03 +0300

    Added route to the Ecommerce app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+
+import {Route} from 'react-router-dom'
 import HomePage from './Pages/Homepage/homepage.component'
+const HatsPage=()=>(
+  <div>
+    <h1> Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop/hats' component={HatsPage}/>
     </div>
   );
 }
--- src/Components/Menu-item/menu-item.component.jsx
+++ src/Components/Menu-item/menu-item.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './menu-item.styles.scss'
-const MenuItem=({title,imageUrl,size})=>(
-    <div className={`${size} menu-item` }>
+import {withRouter}from 'react-router-dom'
+const MenuItem=({title,imageUrl,size,history,linkUrl,match})=>(
+    <div className={`${size} menu-item` } onClick={()=>history.push(`${match.url+linkUrl}`)}>
 
     <div className='background-image' style={{
         backgroundImage:`url(${imageUrl})`
@@ -13,4 +14,4 @@ const MenuItem=({title,imageUrl,size})=>(
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
