5bcd8bb63aa15a7f2b6f121273055a8e928255e9
Joshua Mwakio <mwakiojoshua@hotmail.com>
2020-07-08 15:15:38 +0300

    Rectified selectUser and removed logger during production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,9 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer'
 import {persistStore} from 'redux-persist'
 
-const middleWare=[logger];
+const middleWare=[];
+if(process.env.NODE_ENV==='development'){
+    middleWare.push(logger);
+}
 export const store=createStore(rootReducer,applyMiddleware(...middleWare))
 export const persistor=persistStore(store)
--- src/redux/user/user.selector.js
+++ src/redux/user/user.selector.js
@@ -2,6 +2,6 @@ import {createSelector} from 'reselect'
 const selectUser=state=>state.user;
 
 export const selectCurrentUser=createSelector(
-    [selectUser], cart=>cart.hidden
+    [selectUser], user=>user.currentUser
 )
 
